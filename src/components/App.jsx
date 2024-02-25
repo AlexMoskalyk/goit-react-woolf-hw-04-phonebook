898e0672fbd82a701c224e0b996c8871038c8fc9
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2024-02-25 14:49:55 +0100

    added functionality regarding localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Section from './section/Section';
 import Form from './form/Form';
 import ContactList from './contactList/ContactList';
@@ -7,14 +7,23 @@ import { nanoid } from 'nanoid';
 import Filter from './filter/Filter';
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(getContactsFromLocalStorage());
 
   const [filter, setFilter] = useState('');
+
+  function getContactsFromLocalStorage() {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      return JSON.parse(storedContacts);
+    } else {
+      return [];
+    }
+  }
+
+  useEffect(
+    () => localStorage.setItem('contacts', JSON.stringify(contacts)),
+    [contacts]
+  );
 
   const handleFilter = evt => {
     setFilter(evt.target.value);
